fccb288a38a5b80141a79c942e19e2b7471489af
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-10-26 04:34:40 +0530

    Update streamlit_app.py
    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.header('Menu')
 streamlit.text('🥗 Salad')
 streamlit.text('🍞 Bread')
 streamlit.text('🥣 Curry')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
